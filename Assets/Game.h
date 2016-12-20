afe43230f736b0596edbfec5737f7e8614d000b9
Wesley Carlton <yusaku34@noreply.github.com>
2016-12-20 13:52:07 -0500

    Master Chili

---
+++ Assets/Game.h
@@ -0,0 +1,48 @@
+/****************************************************************************************** 
+ *	Chili DirectX Framework Version 12.04.24											  *	
+ *	Game.h																				  *
+ *	Copyright 2012 PlanetChili.net														  *
+ *																						  *
+ *	This file is part of The Chili DirectX Framework.									  *
+ *																						  *
+ *	The Chili DirectX Framework is free software: you can redistribute it and/or modify	  *
+ *	it under the terms of the GNU General Public License as published by				  *
+ *	the Free Software Foundation, either version 3 of the License, or					  *
+ *	(at your option) any later version.													  *
+ *																						  *
+ *	The Chili DirectX Framework is distributed in the hope that it will be useful,		  *
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of						  *
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the						  *
+ *	GNU General Public License for more details.										  *
+ *																						  *
+ *	You should have received a copy of the GNU General Public License					  *
+ *	along with The Chili DirectX Framework.  If not, see <http://www.gnu.org/licenses/>.  *
+ ******************************************************************************************/
+#pragma once
+
+#include "D3DGraphics.h"
+#include "Keyboard.h"
+#include "Mouse.h"
+#include "Sound.h"
+
+class Game
+{
+public:
+	Game( HWND hWnd,const KeyboardServer& kServer,const MouseServer& mServer );
+	void Go();
+private:
+	void ComposeFrame();
+	/********************************/
+	/*  User Functions              */
+
+	/********************************/
+private:
+	D3DGraphics gfx;
+	KeyboardClient kbd;
+	MouseClient mouse;
+	DSound audio;
+	/********************************/
+	/*  User Variables              */
+
+	/********************************/
+};
